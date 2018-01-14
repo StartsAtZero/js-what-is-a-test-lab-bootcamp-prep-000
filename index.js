7f95236d394af183638db93774ee58e2fe024414
Alevtina Lobova <ally1125@gmail.com>
2018-01-14 22:04:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var name = "Susan"
 var height = 39
 var message = ` is ${height} inches tall`
-if(height < 40){name="Susan"
-
+if(height < 40){name="Susan";
+}
+else
 module.exports = {name, height, message}
