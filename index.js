e7291adc675c1453218dfa83a7b8dd9e447e4583
Alevtina Lobova <ally1125@gmail.com>
2018-01-14 22:06:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
-var name = "Joe"
+var name = "Susan"
 var height = 39
-var message = ` is ${height} inches tall`
+var message = `${name} is ${height} inches tall`
 if (height < 40){name="Susan";
 }
 else {name="Joe"
